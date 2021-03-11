bad7ea7023263cd79fd04cc707a58e398a4e8096
IzumiSagiri900928 <79688664+IzumiSagiri900928@users.noreply.github.com>
2021-03-11 22:28:16 +0800

    Add files via upload

--- 指標.cpp
+++ 指標.cpp
@@ -4,15 +4,15 @@ int main()
     int n[3]={10,20,30};
     printf("n[0]:%d n[1]:%d n[2]:%d\n",n[0],n[1],n[2]);
 
-    int *p=&n[0];
+    int *p=&n[0];///利用 int *p 來改變 n1變數的值
     *p = 200;
     printf("n[0]:%d n[1]:%d n[2]:%d\n",n[0],n[1],n[2]);
 
-    int *p2=&n[2];
+    int *p2=&n[2];///利用 int *p2 來改變 n2 變數的值
     *p2 = 300;
     printf("n[0]:%d n[1]:%d n[2]:%d\n",n[0],n[1],n[2]);
 
-     p2 = p;
+     p2 = p;         ///利用 p2=p 讓口袋的address位址換了一張, 所以 *p2=400 就會換到其他地方送400元
     *p2 = 400;
     printf("n[0]:%d n[1]:%d n[2]:%d\n",n[0],n[1],n[2]);
 }
