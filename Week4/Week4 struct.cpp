570565725e2697f7a1b76db06ab26d0216675dbb
IzumiSagiri900928 <79688664+IzumiSagiri900928@users.noreply.github.com>
2021-03-19 13:39:26 +0800

    Update Week4 struct.cpp

--- Week4/Week4 struct.cpp
+++ Week4/Week4 struct.cpp
@@ -10,20 +10,7 @@ int main()
         struct  POINT a;
 }
 ```
-## 2. 老師利用 C Tutor 及 CodeBlocks 示範 struct POINT a={4.1, 3.2}; 並利用 a.x 及 a.y 印出來。
-```C
-#include <stdio.h>
-struct POINT{
-    float x, y;
-};
 
-int main()
-{
-    struct  POINT a{ 4.1, 3.2};
-    printf("%f %f",a.x,a.y);
-    return 0;
-}
-```
 ## 3.老師利用 C Tutor 及 CodeBlocks 示範, 接續上題, 再利用 a.x 及 a.y 來改變裡面的值。
 ```C
 #include <stdio.h>
