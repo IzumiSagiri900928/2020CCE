87232fccf0a85de610f1572ba6219a8745a919de
IzumiSagiri900928 <79688664+IzumiSagiri900928@users.noreply.github.com>
2021-03-19 11:42:31 +0800

    Update Week4 struct.cpp

--- Week4 struct.cpp
+++ Week4 struct.cpp
@@ -1,4 +1,4 @@
-## 1
+## 1. 老師利用課本&FB貼文, 示範 struct POINT{ float x, y; }; 的宣告定義。請練習寫 struct的宣告、定義, 並宣告一個 struct 變數
 ```C
 #include <stdio.h>
 struct POINT{
@@ -10,7 +10,7 @@ int main()
         struct  POINT a;
 }
 ```
-## 2
+## 2. 老師利用 C Tutor 及 CodeBlocks 示範 struct POINT a={4.1, 3.2}; 並利用 a.x 及 a.y 印出來。
 ```C
 #include <stdio.h>
 struct POINT{
@@ -24,7 +24,7 @@ int main()
     return 0;
 }
 ```
-## 3
+## 3.老師利用 C Tutor 及 CodeBlocks 示範, 接續上題, 再利用 a.x 及 a.y 來改變裡面的值。
 ```C
 #include <stdio.h>
 struct POINT{
@@ -40,7 +40,7 @@ int main()
     return 0;
 }
 ```
-## 4
+## 4. 重新來一次, 老師利用 C Tutor 及 CodeBlocks(配合printf) 示範 外面的global全域變數 會初始值, 裡面的 local區域變數會是亂碼。 然後我們把 local變數裡面用程式來塞值。
 ```C
 #include <stdio.h>
 struct DATA{
@@ -61,7 +61,7 @@ int main()
     printf("c: %d %d\n",c.x ,c.y);
 }
 ```
-## 5
+## 5.重新來一次, 老師利用 C Tutor 及 CodeBlocks(配合printf) 示範, (結構)有個陣列、有個指標, 使用 p->x, p->y, p->z 取用到指標指到的值
 ```C
 #include <stdio.h>
 struct POINT {
